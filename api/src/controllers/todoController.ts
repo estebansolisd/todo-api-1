42bdce198c0704d25531b976ee70b31d351263c2
Esteban Solis <estebansolisd@gmail.com>
2024-11-22 08:56:56 -0600

    Adding correct validations

--- api/src/controllers/todoController.ts
+++ api/src/controllers/todoController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import Database from "../data/db";
+import { validationResult } from "express-validator";
 
-const db  = new Database();
+const db = new Database();
 
 export const getTodos = async (req: Request, res: Response) => {
   res.json(db.getTodos());
@@ -9,7 +10,7 @@ export const getTodos = async (req: Request, res: Response) => {
 
 export const getTodoById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const todo = db.getTodosById(Number(id))
+  const todo = db.getTodosById(Number(id));
   if (todo) {
     res.json(todo);
   } else {
@@ -18,18 +19,30 @@ export const getTodoById = async (req: Request, res: Response) => {
 };
 
 export const createTodo = async (req: Request, res: Response) => {
-  const newTodo = {...req.body, id: Math.floor(Math.random() * 1000000)}
-  db.createTodo(newTodo)
+  const newTodo = { ...req.body, id: Math.floor(Math.random() * 1000000) };
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  db.createTodo(newTodo);
   res.status(201).json(newTodo);
 };
 
 export const updateTodo = async (req: Request, res: Response) => {
   const { id } = req.params;
   const numId = Number(id);
-  const todo = db.getTodosById(numId)
+  const todo = db.getTodosById(numId);
 
   if (todo) {
-    const updatedTodo = db.updateTodo(numId, req.body)
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const updatedTodo = db.updateTodo(numId, req.body);
     res.json(updatedTodo);
   } else {
     res.status(404).json({ message: "Todo not found" });
@@ -39,9 +52,9 @@ export const updateTodo = async (req: Request, res: Response) => {
 export const deleteTodo = async (req: Request, res: Response) => {
   const { id } = req.params;
   const numId = Number(id);
-  const todo = db.getTodosById(numId)
+  const todo = db.getTodosById(numId);
   if (todo) {
-   db.deleteTodosById(numId)
+    db.deleteTodosById(numId);
     res.status(204).json();
   } else {
     res.status(404).json({ message: "Todo not found" });
--- api/src/routes/todoRoutes.ts
+++ api/src/routes/todoRoutes.ts
@@ -6,12 +6,23 @@ import {
   getTodoById,
   updateTodo,
 } from "../controllers/todoController";
+import { body } from "express-validator";
 
 const router = Router();
 
+
+
+const validatePostBody = [
+  body("completed").isBoolean().withMessage("Completed must be a boolean"),
+  body("title")
+    .isString()
+    .notEmpty()
+    .withMessage("Title is required and must be a string"),
+];
+
 router.get("/tasks", getTodos);
-router.post("/tasks", createTodo);
-router.put("/tasks/:id", updateTodo);
+router.post("/tasks", validatePostBody, createTodo);
+router.put("/tasks/:id", validatePostBody, updateTodo);
 router.delete("/tasks/:id", deleteTodo);
 router.get("/tasks/:id", getTodoById);
 
